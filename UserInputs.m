83ceef14da978af7bb6643eb301082e897adb711
Luke <lukehendrikse@cmail.carleton.ca>
2023-11-19 17:37:15 -0500

    simulation is now functional. PID gains need to be adjusted to optimize flight time

--- UserInputs.m
+++ UserInputs.m
@@ -11,34 +11,22 @@ clear all;
 
 userInputs = struct;
 
+%gravity
+Gravity = 9.81;
+
+%drone parameters
+userInputs.dParams.m = 4.5; %kg
+userInputs.dParams.vMax = 16; %m/s
+userInputs.dParams.opsTime = 59*60; %secs
+userInputs.dParams.opsAlt = 160; %m
+userInputs.dParams.maxClimb = 3; %m/s
+userInputs.dParams.dragCoef = 0.09;
+userInputs.dParams.Aref = 0.08319; %m^2
+userInputs.dParams.maxT = 1.174; %N
+
 %Control Commands timeseries creation
-userInputs.commands = struct;
+userInputs.commands = load('badWaypoints.mat');
 
-%Command flags
-userInputs.commands.commandFlags = struct;
-userInputs.commands.commandFlags.thrustEnable = 1;
-userInputs.commands.commandFlags.psiEnable = 0;
-userInputs.commands.commandFlags.thetaEnable = 0;
-userInputs.commands.commandFlags.phiEnable = 0;
-
-%Thrust Command
-userInputs.commands.thrust = timeseries;
-userInputs.commands.thrust.data = [0; 10; 0];
-userInputs.commands.thrust.time = [0; 10; 80];
-
-%Euler angles in the body frame
-%ZYX order; psi = yaw (rate) , theta = pitch, phi = roll
-userInputs.commands.psi = timeseries;
-userInputs.commands.psi.data = [0; 0];
-userInputs.commands.psi.time = [0; 10];
-
-userInputs.commands.theta = timeseries;
-userInputs.commands.theta.data = [0; 0];
-userInputs.commands.theta.time = [0; 10];
-
-userInputs.commands.phi = timeseries;
-userInputs.commands.phi.data = [0; 0];
-userInputs.commands.phi.time = [0; 10];
 
 %visualization
 userInputs.Visualization = struct;
